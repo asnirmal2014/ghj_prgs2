7570357246e8d453fc770f3587a0b1bb3f65825b
user <user@192.168.0.3>
2020-07-27 19:01:48 +0530

    C

--- src/com/pkg/t1/Manager1.java
+++ src/com/pkg/t1/Manager1.java
@@ -6,6 +6,6 @@ public class Manager1
   {
 	 System.out.println("Test1");
 	 System.out.println("Test2"); 
-	 System.out.println("Test3");    
+	 System.out.println("Test3");  // Recently Updated    
   }
 }
