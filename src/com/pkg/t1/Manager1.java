0b9881109b8e73312630b46e9ffeaa36dfa9da21
qcgittest2014 <32424198+asnirmal2014@users.noreply.github.com>
2020-07-28 08:31:24 +0530

    Update Manager1.java
    C

--- src/com/pkg/t1/Manager1.java
+++ src/com/pkg/t1/Manager1.java
@@ -7,6 +7,7 @@ public class Manager1
 	 System.out.println("Test1");
 	 System.out.println("Test2"); 
 	 System.out.println("Test3");       
-	 System.out.println("Perform Pull Request");
+	 System.out.println("Perform Pull Request"); 
+	 System.out.println("Perform Pull Request imediatly");  
   }
 }
